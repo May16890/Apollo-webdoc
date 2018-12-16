b96bda93e234839a285e70f448c7138cae1b012a
mallano <maelallano1@gmail.com>
2018-12-16 21:50:25 +0100

    black filter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.scss';
+import HomeContainer from '../Home/HomeContainer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Router>
+          <Switch>
+            <Route exact path='/' component={HomeContainer}/>
+          </Switch>
+        </Router>
       </div>
     );
   }
