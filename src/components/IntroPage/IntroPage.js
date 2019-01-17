b03f4df959bea6f49f9125b257a48fc2a01828fc
mallano <maelallano1@gmail.com>
2019-01-17 03:31:46 +0100

    introPage done

--- src/components/IntroPage/IntroPage.js
+++ src/components/IntroPage/IntroPage.js
@@ -3,22 +3,33 @@ import './IntroPage.scss';
 import { Link } from 'react-router-dom';
 import Timeline from '../Timeline/Timeline';
 import bgIntro from '../../assets/img/Galaxy.png';
+import astroImg from '../../assets/img/Astronaute.png';
 
 class IntroPage extends Component {
   render() {
     return (
       <section className="intro">
-        {/* <Link to='/'>BACK HOME</Link>
-        <hr />
-        <Link to='/navettes'>NAVETTES</Link>
-        <hr />
-        <Link to='/astronautes'>ASTRONAUTES</Link> */}
-        <h1 style={{ color: 'black', position: 'absolute', bottom: 10, left: 10}}>INTRO</h1>
         <div className="blackOverlay"></div>
         <img className="bgIntro" alt="bgIntro" src={bgIntro}/>
-        <div className="intro__timelineContainer">
-          <Timeline />
+        <Timeline />
+        <div className='intro__introContainer'>
+          <div className='intro__introContainer__wrapper'>
+            <h1 className='intro__introContainer__title'>BIENVENUE À BORD</h1>
+            <h2 className='intro__introContainer__subtitle'>APOLLO VOUS ATTENDS</h2>
+            <p className='intro__introContainer__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br />
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim <br /> 
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea <br />
+              commodo consequat.</p>
+            <div className='intro__introContainer__linksContainer'>
+              <Link className='intro__introContainer__links' to='/astronautes'>ASTRONAUTES</Link>
+              <Link className='intro__introContainer__links' to='/navettes'>NAVETTES</Link>
+            </div>
+          </div>
+          <div className='intro__introContainer__introOverlay'>
+
+          </div>
         </div>
+        <img className="astroImg" alt="astroImg" src={astroImg}/>
       </section>
     );
   }
--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,30 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Timeline.scss';
 import TimelinePeriod from '../TimelinePeriod/TimelinePeriod.js';
 import TimelineDate from '../TimelineDate/TimelineDate.js';
 
-const Timeline = () => (
-    <div className='timelineContainer'>
-        <div className='timeline'>
-            <TimelineDate date='1950' />
-            <TimelinePeriod text='Premice' link='/singlePage/premice'/>
-            <TimelineDate date='1966' />
-            <TimelinePeriod text='Apollo 1' link='/singlePage/apollo1'/>            
-            <TimelineDate date='1969' />
-            <TimelinePeriod text='Apollo 2' link='/singlePage/apollo2'/>
-            <TimelineDate date='1969' />
-            <TimelinePeriod text='Apollo 3' link='/singlePage/apollo3'/>
-            <TimelineDate date='1972' />
-            <TimelinePeriod text='Apollo Soyuz' link='/singlePage/apolloSoyuz'/>
-            <TimelineDate date='1975' />
-            <TimelinePeriod text='Today' link='/singlePage/today'/>
-            <TimelineDate date='2019' />
-            <div className='timeline__closeBtn'>
-                <div className='timeline__closeBtn__cross1'></div>
-                <div className='timeline__closeBtn__cross2'></div>
+class Timeline extends Component {
+    state = {
+        isClosed: false,
+    }
+
+    closeTimeline = () => {
+        this.setState({
+            isClosed: true,
+        })
+    }
+
+    openTimeline = () => {
+        this.setState({
+            isClosed: false,
+        })
+    }
+
+    toggleTimeline = () => {
+        if (!this.state.isClosed) {
+            this.closeTimeline();
+            return null;
+        }
+        this.openTimeline();
+    }
+
+    render() {
+        const { isClosed } = this.state;
+        const toggleBtn = !isClosed ? 
+            (<div className='timeline__toggleBtn' onClick={this.toggleTimeline}>
+                <div className='timeline__toggleBtn__cross1'></div>
+                <div className='timeline__toggleBtn__cross2'></div>
+            </div>) : 
+            (<div className='timeline__openBtnContainer' onClick={this.toggleTimeline}>
+                <div className='timeline__openBtn'>
+                    <div className='timeline__openBtn__arrow1'></div>
+                    <div className='timeline__openBtn__arrow2'></div>
+                </div>
+                <p className='timeline__openBtn__text'>TIMELINE</p>
+            </div>);
+        return (
+            <div className={`timelineBlackContainer`}>
+                <div className={`timelineContainer ${this.state.isClosed ? 'hidden' : ''}`}>
+                    <div className='timeline'>
+                        <TimelineDate date='1950' />
+                        <TimelinePeriod text='Premice' link='/singlePage/premice'/>
+                        <TimelineDate date='1966' />
+                        <TimelinePeriod text='Apollo 1' link='/singlePage/apollo1'/>            
+                        <TimelineDate date='1969' />
+                        <TimelinePeriod text='Apollo 2' link='/singlePage/apollo2'/>
+                        <TimelineDate date='1969' />
+                        <TimelinePeriod text='Apollo 3' link='/singlePage/apollo3'/>
+                        <TimelineDate date='1972' />
+                        <TimelinePeriod text='Apollo Soyuz' link='/singlePage/apolloSoyuz'/>
+                        <TimelineDate date='1975' />
+                        <TimelinePeriod text='Today' link='/singlePage/today'/>
+                        <TimelineDate date='2019' />
+                        {toggleBtn}
+                        {/* <div className='timeline__closeBtn' onClick={this.toggleTimeline}>
+                            <div className='timeline__closeBtn__cross1'></div>
+                            <div className='timeline__closeBtn__cross2'></div>
+                        </div> */}
+                    </div>
+                </div>
+                {/* <div className={`timeline__openBtn ${this.state.isClosed ? 'showOpenButton' : ''}`} onClick={this.openTimeline}>
+                    <div className='timeline__openBtn__cross1'></div>
+                    <div className='timeline__openBtn__cross2'></div>
+                </div> */}
             </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 export default Timeline;
