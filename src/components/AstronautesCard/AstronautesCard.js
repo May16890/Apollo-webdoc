d74b88877f9040da0fc61ef692508311d7bbf384
Nancy_Cpbl <nancy.campbell@hetic.net>
2019-02-04 23:09:56 +0100

    astronautes data structure + link with singlePages

--- src/components/AstronautesCard/AstronautesCard.js
+++ src/components/AstronautesCard/AstronautesCard.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './AstronautesCard.scss';
 import astronautePicture from '../../assets/img/astronautePicture.jpg';
 
-const handleClick = e => {
-    console.log(e.type);
-}
 
-const AstronautesCard = (props) => {
+// const handleClick = e => {
+//     console.log(e.type);
+// }
+
+const AstronautesCard = (data) => {
+    const { src, name, mission, date, target } = data;
     return (
         <div className="astronautesPage__cardsContainer__wrapper">
-            <img className="astronautePictures" alt="astronautePicture" src={astronautePicture}></img>
-            <h3>Harrison Schmitt{props.name}</h3>
-            <h3>April 1966{props.date}</h3>
-            <p>Apollo 11{props.mission}</p>
-            <button onClick={e => handleClick(e)}>CLICK</button>
+            <img className="astronautePictures" alt="astronautePicture" src={src}></img>
+            <h3>{name}</h3>
+            <h3>{date}</h3>
+            <p>{mission}</p>
+            <Link to={{ pathname: `/singlePage/${target[0]}`, state: { currentCard: target[1]}}}>CLICK</Link>
         </div>
     )
 }
