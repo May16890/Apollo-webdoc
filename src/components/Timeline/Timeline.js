dc929e1f2cdd9daf3fbdce617965d45ccfb1d60b
Nancy_Cpbl <nancy.campbell@hetic.net>
2019-02-07 22:29:44 +0100

    add content for first and second period

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -75,13 +75,13 @@ class Timeline extends Component {
                         <TimelineDate color={color} date='1950' isRed={(targets[0] === 1 || targets[1]  === 1)}/>
                         <TimelinePeriod color={color} text='Premice' link='/singlePage/premice' targets={[1, 2]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
                         <TimelineDate color={color} date='1966' isRed={(targets[0] === 2 || targets[1]  === 2)}/>
-                        <TimelinePeriod color={color} text='Apollo 1' link='/singlePage/apollo1' targets={[2, 3]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
+                        <TimelinePeriod color={color} text='Début du programme' link='/singlePage/apollo1' targets={[2, 3]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
                         <TimelineDate color={color} date='1969' isRed={(targets[0] === 3 || targets[1]  === 3)}/>
-                        <TimelinePeriod color={color} text='Apollo 2' link='/singlePage/apollo2' targets={[3, 4]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
+                        <TimelinePeriod color={color} text='Apollo 11' link='/singlePage/apollo2' targets={[3, 4]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
                         <TimelineDate color={color} date='1969' isRed={(targets[0] === 4 || targets[1]  === 4)}/>
-                        <TimelinePeriod color={color} text='Apollo 3' link='/singlePage/apollo3' targets={[4, 5]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
+                        <TimelinePeriod color={color} text='Fin du programme' link='/singlePage/apollo3' targets={[4, 5]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
                         <TimelineDate color={color} date='1972' isRed={(targets[0] === 5 || targets[1]  === 5)}/>
-                        <TimelinePeriod color={color} text='Apollo Soyuz' link='/singlePage/apolloSoyuz' targets={[5, 6]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
+                        <TimelinePeriod color={color} text='Apollo-Soyouz' link='/singlePage/apolloSoyuz' targets={[5, 6]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
                         <TimelineDate color={color} date='1975' isRed={(targets[0] === 6 || targets[1]  === 6)}/>
                         <TimelinePeriod color={color} text='Today' link='/singlePage/today' targets={[6, 7]} setTargets={this.setTargets} removeTargets={this.removeTargets}/>
                         <TimelineDate color={color} date='2019'  isRed={(targets[0] === 7 || targets[1]  === 7)}/>
--- src/datas/singlePage.js
+++ src/datas/singlePage.js
@@ -1,17 +1,17 @@
 import testImg from '../assets/img/4_19.jpg';
-import astro1Apollo13 from '../assets/img/apollo13/astro1Apollo13.png';
-import astro2Apollo13 from '../assets/img/apollo13/astro2Apollo13.png';
-import astro3Apollo13 from '../assets/img/apollo13/astro3Apollo13.png';
+//import astro1Apollo13 from '../assets/img/apollo13/astro1Apollo13.png';
+//import astro2Apollo13 from '../assets/img/apollo13/astro2Apollo13.png';
+//import astro3Apollo13 from '../assets/img/apollo13/astro3Apollo13.png';
 import astroAllApollo13 from '../assets/img/apollo13/astroAllApollo13.png';
-import logoApollo13 from '../assets/img/apollo13/logoApollo13.png';
-import navetteApollo13 from '../assets/img/apollo13/navetteApollo13.png';
+//import logoApollo13 from '../assets/img/apollo13/logoApollo13.png';
+//import navetteApollo13 from '../assets/img/apollo13/navetteApollo13.png';
 import guerreFroide from '../assets/img/guerre_froide.png';
 import intro2 from '../assets/img/wernher.png';
 import GFLogo from '../assets/img/GF_logo.png';
 import USAURSS from '../assets/img/URSS_USA.png';
 import moon from '../assets/img/moon.png';
 import AtlasContent from '../assets/img/Atlas_content.png';
-import ImgIntro2 from '../assets/img/intro2.png';
+//import ImgIntro2 from '../assets/img/intro2.png';
 import Journal from '../assets/img/tintin_journal.png';
 import GeminiPatch from '../assets/img/geminiPatch.png';
 import MercuryPatch from '../assets/img/mercuryPatch.png';
@@ -23,6 +23,9 @@ import Juno from '../assets/img/Juno_mission.svg';
 import MSLPic1 from '../assets/img/MSL_cruise.png';
 import MSLPic2 from '../assets/img/MSL_pic.jpg';
 import Osiris from '../assets/img/Osiris.png';
+import Apollo7 from '../assets/img/Apollo7_logo.png';
+import Apollo9 from '../assets/img/Apollo-9.png';
+import Apollo11 from '../assets/img/Apollo_11.png';
 
 const erasDatas = {
     premice: [
@@ -77,38 +80,60 @@ const erasDatas = {
         },
     ],
     apollo1: [
-        [
-            ['text', 'jkdsjdkjsd djksqjdksqjdks jdskjdksjqdkqsjd jdksqjdksqjdsq jkdsjqkdjqsk.'],
-            ['img', testImg],
-            ['img', testImg],
-            ['text', 'azzazaza zazazazaz aza zaza zaza zazaza zazaza.'],
-        ],
-        [
-            ['text', 'jkdsjdkjsd djksqjdksqjdks jdskjdksjqdkqsjd jdksqjdksqjdsq jkdsjqkdjqsk.'],
-            ['img', testImg],
-            ['img', testImg],
-            ['text', 'azzazaza zazazazaz aza zaza zaza zazaza zazaza.'],
-        ],
+        {
+            'logo': Apollo7,
+            'titleCard': `Apollo 7 et 8`,
+            'intro': `Apollo 7 est la première mission lunaire habitée. Son but était de valider les avancées techniques réalisées suite à l'échec d'Apollo 1. Au cours de son séjour en orbite, l’équipage avec Walter Schirra comme commandant de bord, teste les manœuvres qui seront utilisées lors des missions lunaires.`,
+            'content': `Après avoir quitté l'orbite terrestre et effectué leur rentrée dans l'atmosphère, la capsule et son équipage sont récupérés sans incident dans l'Atlantique. C'était aussi la première mission américaine à envoyer une équipe de trois hommes dans l'espace ainsi qu'à diffuser des images pour la télévision.
+            <br><br/>
+            <br><br/>
+            Le 21 décembre 1968 est lancée la mission Apollo 8 avec Frank Borman  comme commandant de bord. Elle est la première mission du programme a quitter l'orbite terrestre pour se rendre sur celle de la lune.
+            <br><br/>
+            La fusée spatiale Saturn V utilisée pour mettre en orbite Apollo 8 fut désignée SA-503. Il s'agissait du troisième exemplaire construit. 
+            Les astronautes firent au total 10 révolutions autour du satellite terrestre. Durant leur expédition, ils purent prendre différents clichés dont le premier d'un lever de Terre. Apollo 8 permit à l'Homme de voir directement la «face cachée» de la Lune. L'une des tâches dédiées à l'équipage consistait à effectuer une reconnaissance en vue d'un atterrissage, surtout de la mer de la Tranquillité où devait se poser Apollo 11.`,
+            'navetteName': 'GEMINI',
+            
+        },
+        {
+            'logo': Apollo9,
+            'titleCard': `Apollo 9 et 10`,
+            'intro': `Apollo 9 forme le premier test en vol de la totalité des équipements prévus pour une mission lunaire, qui porte à la fois sur le CSM, le Lem et le lanceur Saturn. Pour la première fois, on baptise le CSM (Gumdrop) et le Lem (Spider).`,
+            'content': `L'équipage effectue l'ensemble des manœuvres de la mission lunaire. En orbite terrestre, il simule un atterrissage du Lem et réalise le premier rendez-vous réel entre Lem et CSM. Les astronautes effectuent aussi une sortie extra-véhiculaire de 56 minutes pour tester un transfert d'équipage du Lem au CSM par l'extérieur.<br><br/> En outre, ils testent des manœuvres de secours, entre autres une procédure durant laquelle les astronautes pourraient utiliser&nbsp;le Lem comme «canot de sauvetage» au cas où le module de commande deviendrait non manœuvrable ou inhabitable ; c'est cette procédure qui sera ensuite utilisée pour récupérer l'équipage d'Apollo 13.
+            <br><br/>
+            <br><br/>
+            Apollo 10 cette mission était avant tout une répétition pour la phase qui précédait l'atterrissage. Le module lunaire, surnommé «Snoopy», fut envoyé à 15, 6 km de la surface lunaire. En dehors de cela, la mission reproduisait les principales étapes du vol final, à la fois dans l'espace et au sol. Peu après avoir quitté son orbite terrestre basse, le module de commande se sépara du troisième étage de Saturn V, effectua une rotation à 180° puis arrima son nez au sommet du module lunaire, lequel était toujours attaché à la fusée Saturn.<br><br/>
+            La totalité module de service et module lunaire se sépara ensuite du lanceur. Les moteurs du module de commande propulsèrent les deux vaisseaux jusqu'à la Lune. John W. Young prit les commandes du CSM tandis que Thomas Stafford et Eugene Cernan prenaient le contrôle du module lunaire. 
+            Ils vérifièrent surtout son radar et son moteur d'ascension, mais aussi le site d'atterrissage dans la mer de la Tranquillité.
+                        `,
+            'navetteName': 'GEMINI',
+            
+        },
     ],
     apollo2: [
-        [
-            ['text', 'jkdsjdkjsd djksqjdksqjdks jdskjdksjqdkqsjd jdksqjdksqjdsq jkdsjqkdjqsk.'],
-            ['img', testImg],
-            ['img', testImg],
-            ['text', 'azzazaza zazazazaz aza zaza zaza zazaza zazaza.'],
-        ],
-        [
-            ['text', 'jkdsjdkjsd djksqjdksqjdks jdskjdksjqdkqsjd jdksqjdksqjdsq jkdsjqkdjqsk.'],
-            ['img', testImg],
-            ['img', testImg],
-            ['text', 'azzazaza zazazazaz aza zaza zaza zazaza zazaza.'],
-        ],
-        [
-            ['text', 'jkdsjdkjsd djksqjdksqjdks jdskjdksjqdkqsjd jdksqjdksqjdsq jkdsjqkdjqsk.'],
-            ['img', testImg],
-            ['img', testImg],
-            ['text', 'azzazaza zazazazaz aza zaza zaza zazaza zazaza.'],
-        ],
+        {
+            'logo': Apollo11,
+            'titleCard': `Apollo 11`,
+            'intro': `Le 21 juillet 1969 restera une date clé. En effet ce jour-là, l'Homme a marché sur la Lune pour la première fois ! La mission des deux astronautes Neil Armstrong et Edwin Aldrin va rentrer dans l'histoire. Armstrong s'élance et déclare après son premier pas sur la Lune : « C'est un petit pas pour l'homme (que je viens de faire), mais c'est un bond de géant pour l'Humanité ».`,
+            'content': `Armstrong et Aldrin installèrent ensuite un réflecteur laser (sa fonction est d'envoyer des ondes vers la Terre pour mesurer avec une précision de 45 m la distance Terre-Lune), un sismographe ultra-sensible (cet appareil enverra un signal électrique dès le moindre tremblement) et déroulèrent une feuille d'aluminium conçue pour capter les particules solaires. C'est à 14h32 (Heure Française) que la capsule spatiale Apollo XI est partie de Cap Kennedy, au sommet d'une fusée Saturn V de 111 m de haut, pesant 3100 tonnes et d'une puissance de 155 millions de chevaux.
+            <br><br/>
+            <br><br/>
+            "Apollo XI" doit, pour la première fois dans l'histoire de l'Humanité, amener des hommes sur la lune. Le 19 Juillet, "Apollo XI" s'est satellisée avec son équipage composé de Neil Armstrong, Edwin Aldrin et Michael Collins autour de la Lune. Après une vérification approfondie de toutes les installations techniques, la séparation du module d'exploration lunaire (LEM) "Eagle" s'effectue le 20 Juillet, emportant Armstrong et Aldrin vers leur objectif, Collins restant seul aux commandes d' Apollo XI en orbite autour de la Lune. Après une manoeuvre plus que délicate, le LEM touche la surface lunaire à 21 h 17. Après un séjour de quelques heures dans le LEM, la sortie se prépare. Le premier homme à poser le pied sur la lune, Neil Armstrong, accomplit le geste historique le 21 Juillet à 3 h 56 et prononce sa phrase devenue célèbre depuis : "C'est un petit pas pour l'homme, un bond de géant pour l'Humanité".            <br><br/>
+            La fusée spatiale Saturn V utilisée pour mettre en orbite Apollo 8 fut désignée SA-503. Il s'agissait du troisième exemplaire construit. 
+            Les astronautes firent au total 10 révolutions autour du satellite terrestre. Durant leur expédition, ils purent prendre différents clichés dont le premier d'un lever de Terre. Apollo 8 permit à l'Homme de voir directement la «face cachée» de la Lune. L'une des tâches dédiées à l'équipage consistait à effectuer une reconnaissance en vue d'un atterrissage, surtout de la mer de la Tranquillité où devait se poser Apollo 11.`,
+            'navetteName': 'GEMINI',
+            
+        },
+        {
+            'logo': Apollo11,
+            'titleCard': `Apollo 11`,
+            'intro': `Edwin Aldrin le suit quelques minutes plus tard. Les deux astronautes resteront en tout 22 heures sur la Lune, dont environ 2 à l'extérieur du LEM. Des images des opérations sont transmises en direct de la Lune à la Terre : on estime que 500 millions de téléspectateurs assistent de par le monde aux retransmissions, comme lors de l'envol d' "Apollo XI".`,
+            'content': `Le programme de travail des astronautes sur le sol Lunaire comprend la mise en place de certains dispositifs scientifiques, par exemple pour la mesure du vent solaire, ainsi qu'une série d'exercices de mouvement sur un sol où la gravité ne représente que 20% de celle qui règne sur la Terre. La première conversation téléphonique entre la Lune et la Terre a lieu entre les astronautes et le Président Richard Nixon. Le départ de la Lune a lieu le soir du 21 Juillet à 18 h 54 précises, avec l'allumage de la fusée de l'étage du LEM servant de retour. Le module s'arrime à la capsule mère Columbia à 22 h 35, les deux "hommes de la Lune", Armstrong et Aldrin, rejoignant Collins. Le lendemain "Apollo XI", après le largage de l'étage du LEM, rallume ses moteurs et entame sa route vers la Terre.
+            <br><br/>
+            <br><br/>
+            Le 24 Juillet à 17 h 51, la capsule touche le Pacifique dans la zone prévue : la première expédition humaine sur la Lune s'achève. En souvenir du premier alunissage, les astronautes américains ont laissé sur le sol lunaire un drapeau US en Nylon, ainsi qu'une plaque d’acier inaltérable sur laquelle de futurs "touristes lunaires" pourront lire&nbsp;: "C'est ici que des êtres humains de la planète Terre posèrent pour la première fois le pied sur la Lune, en 1969 après J-C. Nous sommes venus en paix pour toute l'Humanité. Neil A. Armstrong, astronaute, Edwin E. Aldrin, astronaute, Michael Collins, astronaute, Richard Nixon, Président des Etats-Unis d'Amérique".`,
+            'navetteName': 'GEMINI',
+            
+        }
     ],
     apollo3: [
         [
@@ -177,19 +202,19 @@ const erasDatas = {
             
             },
             {
-                'logo': Osiris,
-                'titleCard': `OSIRIS-REx`,
-                'intro': `Arrivée à proximité de  <span style='font-weight: 800'>l’astéroïde Bennu</span> le 3 décembre, la sonde <span style='font-weight: 800'>Osiris-Rex</span> y stationne autour, à seulement une vingtaine de kilomètres de distance. Depuis cette date, elle renvoie des images et des données extraordinaires d'un nouveau monde.`,
-                'content': `Il y a deux jours, l'équipe scientifique de la mission a annoncé les premiers résultats et certains sont « très excitants et enthousiasmants comme la présence de minéraux hydratés, c'est à dire des minéraux qui ont interagi avec l'eau », explique Patrick Michel, astrophysicien, directeur de recherches au CNRS et membre de l'équipe scientifique de la mission.
-                <br><br/>
-                Les observations de Bennu ont débuté dès la phase d'approche et ont continué avec des survols à basses altitudes, à seulement 7 kilomètres de sa surface. Elles ont confirmé ce à quoi les scientifiques s'attendaient en termes « de forme, de masse et de densité ». L'astéroïde ressemble étonnamment au modèle de forme, dérivé des données acquises par le radar d'Arecibo réalisé en 2013. Quant à sa densité (1,19 grammes par centimètres cube), elle est aussi très proche de celle estimée depuis le sol terrestre avec une technique alors inédite qui a consisté à « mesurer l'effet thermique, appelé <span style='font-weight: 800'>Yarkovsky</span>, qui perturbe la trajectoire de l'astéroïde autour du Soleil ». À l'avenir, cette technique validée pourra ainsi être utilisée pour caractériser les astéroïdes les plus proches de la Terre avec des résultats proches de la réalité.
-                <br><br/>
-                <br><br/>
-                Les données spectrales indiquent que Bennu est lié aux <span style='font-weight: 800'>chondrites carbonées</span> de type CM et CI, ce qui en fait un « objet très primitif » dont le corps parent se serait « formé très tôt dans l'histoire de la formation du Système Solaire dans une région riche en eau ». La composition de cet objet d'environ 500 mètres de diamètre est donc « fidèle à celle du matériau présent dans les premières phases de formation du Système solaire il y a 4,6 milliards d'années » ; ce qui va permettre aux scientifiques d'approfondir leurs connaissances sur cette époque.
-                `,
-                'navetteName': 'GEMINI',
-                
-                },
+            'logo': Osiris,
+            'titleCard': `OSIRIS-REx`,
+            'intro': `Arrivée à proximité de  <span style='font-weight: 800'>l’astéroïde Bennu</span> le 3 décembre, la sonde <span style='font-weight: 800'>Osiris-Rex</span> y stationne autour, à seulement une vingtaine de kilomètres de distance. Depuis cette date, elle renvoie des images et des données extraordinaires d'un nouveau monde.`,
+            'content': `Il y a deux jours, l'équipe scientifique de la mission a annoncé les premiers résultats et certains sont « très excitants et enthousiasmants comme la présence de minéraux hydratés, c'est à dire des minéraux qui ont interagi avec l'eau », explique Patrick Michel, astrophysicien, directeur de recherches au CNRS et membre de l'équipe scientifique de la mission.
+            <br><br/>
+            Les observations de Bennu ont débuté dès la phase d'approche et ont continué avec des survols à basses altitudes, à seulement 7 kilomètres de sa surface. Elles ont confirmé ce à quoi les scientifiques s'attendaient en termes « de forme, de masse et de densité ». L'astéroïde ressemble étonnamment au modèle de forme, dérivé des données acquises par le radar d'Arecibo réalisé en 2013. Quant à sa densité (1,19 grammes par centimètres cube), elle est aussi très proche de celle estimée depuis le sol terrestre avec une technique alors inédite qui a consisté à « mesurer l'effet thermique, appelé <span style='font-weight: 800'>Yarkovsky</span>, qui perturbe la trajectoire de l'astéroïde autour du Soleil ». À l'avenir, cette technique validée pourra ainsi être utilisée pour caractériser les astéroïdes les plus proches de la Terre avec des résultats proches de la réalité.
+            <br><br/>
+            <br><br/>
+            Les données spectrales indiquent que Bennu est lié aux <span style='font-weight: 800'>chondrites carbonées</span> de type CM et CI, ce qui en fait un « objet très primitif » dont le corps parent se serait « formé très tôt dans l'histoire de la formation du Système Solaire dans une région riche en eau ». La composition de cet objet d'environ 500 mètres de diamètre est donc « fidèle à celle du matériau présent dans les premières phases de formation du Système solaire il y a 4,6 milliards d'années » ; ce qui va permettre aux scientifiques d'approfondir leurs connaissances sur cette époque.
+            `,
+            'navetteName': 'GEMINI',
+            
+            },
     ]
 };
 
